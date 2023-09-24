618ed8a587e293355a530a2e0869389193af37b8
srijharao <102275868+srijharao@users.noreply.github.com>
2023-09-24 18:47:39 -0400

    Create 141 LinkedListCycle.java
    True if LinkedList has a cycle

---
+++ 141 LinkedListCycle.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+
+        ListNode i=head, j=head;
+
+        while(j != null && j.next != null){
+            i = i.next;
+            j = j.next.next;
+            if(i == j){
+                return true;
+            }
+        }
+        return false;
+    }
+}
