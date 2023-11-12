dd37cbc27202d03bdd5b8cd61de68d47e45022bb
srijharao <102275868+srijharao@users.noreply.github.com>
2023-11-12 00:31:16 -0500

    Create 1. Two Sum.java

---
+++ 1. Two Sum.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+
+        Map<Integer, Integer> map = new HashMap<>();
+        int[] result = new int[2];
+
+        for(int i=0; i< nums.length; i++){
+            if(!map.containsKey(target - nums[i])){
+                map.put(nums[i], i);
+            }
+            else{
+                result[0] = i;
+                result[1] = map.get(target- nums[i]);
+            }
+        }
+        return result;
+    }
+}
