6bcfeb05da45badba06ed5fa5acb246b41eb9b74
Srijha Thammareddy <102275868+srijharao@users.noreply.github.com>
2024-09-28 23:05:07 -0500

    Update 70. Climbing Stairs.py

--- 70. Climbing Stairs.py
+++ 70. Climbing Stairs.py
@@ -1,15 +1,14 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-
-        if(n == 1):
+        if n==1:
             return 1
+        steparr = [0]*n
+        steparr[0] = 1
+        steparr[1] = 2
+        for i in range(2,n):
+            steparr[i] = steparr[i-1] + steparr[i-2]
+        return steparr[n-1]
 
-        mem = [0] * n;
-        mem[0] = 1;
-        mem[1] = 2;
+
+
         
-        i = 2
-        while i < n:
-            mem[i] = mem[i-1] + mem[i-2]
-            i = i+1
-        return mem[n-1]
