53fb64383f1cf8567f162c21213674d44bf7ecd6
Srijha Thammareddy <102275868+srijharao@users.noreply.github.com>
2025-11-26 11:46:59 -0800

    Add comments to Coin Change solution
    Added comments to explain the intuition and time complexity of the coin change algorithm.

---
+++ 322. Coin Change.py
@@ -0,0 +1,12 @@
+class Solution:
+  #intuition: if you know how to get to a value earlier to the amount, you can use +1 to that value , meaning
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = [float(inf)] * (amount+1)
+        dp[0] = 0
+
+        for coin in coins:
+            for i in range(coin, amount+1):
+                dp[i] = min(dp[i], dp[i-coin]+1)
+            
+        return dp[-1] if dp[-1] != float(inf) else -1
+        #O(n*m)
